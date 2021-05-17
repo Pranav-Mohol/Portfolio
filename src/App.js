b04ef95ae4dbadc5e7dacbfa5399d18c0cf7769f
PranavMohol <pranavmohol@gmail.com>
2021-05-17 21:07:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.scss';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="sidebar">
+        <Navbar/>
+      </div>
+
+      <div className="main-content"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+//TODO: Import image avatar
+function Navbar() {
+    return (
+        <div className="Navbar">
+            <nav className="nav">
+                <div className="profile">
+                    <img src="" alt="This is img" />
+                {/*TODO:: import images */}
+                </div>
+                
+                <ul className="nav-items">
+                    <li className="nav-item">
+                        <NavLink to="/" exact activeClassName="active">
+                            Home
+                        </NavLink>
+                    </li>
+
+                    <li className="nav-item">
+                        <NavLink to="/about" exact activeClassName="active">
+                            About
+                        </NavLink>
+                    </li>
+
+                    <li className="nav-item">
+                        <NavLink to="/skills" exact activeClassName="active">
+                            Skills
+                        </NavLink>
+                    </li>
+
+                    <li className="nav-item">
+                        <NavLink to="/exprience" exact activeClassName="active">
+                            Exprience
+                        </NavLink>
+                    </li>
+
+                    <li className="nav-item">
+                        <NavLink to="/projects" exact activeClassName="active">
+                            Projects
+                        </NavLink>
+                    </li>
+
+                    <li className="nav-item">
+                        <NavLink to="/contact" exact activeClassName="active">
+                            Contact
+                        </NavLink>
+                    </li>
+                </ul>
+
+                <footer className="footer">
+                <p>
+                    @2021
+                </p>
+                </footer>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
